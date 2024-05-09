c2d770fd2f61bd9d108682aa2b12b55066b965f4
Dustin Aaron Santos <santosdustinaaron07@gmail.com>
2024-05-09 11:56:59 +0800

    adjusted about page and add animation styles to both landing and about page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+// Typing animation
+document.addEventListener("DOMContentLoaded", function () {
+  const text =
+    "Full-stack Web Developer. Electronics Engineer.  Outside-of-the-box thinker.";
+  const element = document.getElementById("typed-text");
+  element.textContent = text;
+});
+
+// For resume button
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("open-resume").addEventListener("click", function () {
     const link = document.createElement("a");
-    link.href = "../files/test.pdf";
+    link.href = "../files/Resume.pdf";
     link.target = "_blank";
     link.click();
     console.log("Opening resume PDF");
